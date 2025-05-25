8c7e0c19e6faf31399314551594f2f8bd29cd422
Ankur Chouhan <69696296+AnkurChouhan@users.noreply.github.com>
2025-05-25 16:07:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server started on http://localhost:${PORT}`);
 });
